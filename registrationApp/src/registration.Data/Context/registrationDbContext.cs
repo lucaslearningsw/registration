773ac9f02c5aac75502228f2c979e29a25013402
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2023-01-02 09:37:24 -0300

    set default value to string that was not declared in mapping

--- registrationApp/src/registration.Data/Context/registrationDbContext.cs
+++ registrationApp/src/registration.Data/Context/registrationDbContext.cs
@@ -19,6 +19,11 @@ namespace registration.Data.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(registrationDbContext).Assembly);
 
