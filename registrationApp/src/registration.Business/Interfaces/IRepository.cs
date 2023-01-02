017cd909ee53d8918a41d0783ea92ecb5197a01a
Lucas Alixandre Lima <lucaslearningsw@gmail.com>
2023-01-02 20:58:27 -0300

    created generic repository

---
+++ registrationApp/src/registration.Business/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using BasicMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace registration.Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);   
+        Task DeleteAsync(TEntity entity);  
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetById(Guid id);
+
+        Task<int> SaveChanges();
+        
+    }
+}
