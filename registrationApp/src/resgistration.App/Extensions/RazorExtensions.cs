f9b7d831b53a3cbef2e79322915aac715407dddb
Lucas Alixandre Lima <lucaslearningsw@gmail.com>
2023-01-30 12:14:10 -0300

    improved views

---
+++ registrationApp/src/resgistration.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace resgistration.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string DocumentFormart(this RazorPage page,int personType, string document)
+        {
+            return personType == 1 ? Convert.ToUInt64(document).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(document).ToString(@"00\.000\.000\/000\-00");
+        }
+    }
+}
