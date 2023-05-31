053f198e8cd3c874735e9c35e8d6d42dae6b32ec
Lucas Alixandre Lima <lucaslearningsw@gmail.com>
2023-05-31 12:59:15 -0300

    crerated a emailTagHelper

---
+++ registrationApp/src/resgistration.App/Extensions/EmailTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace resgistration.App.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+
+        public string EamilDomain { get; set; } = "testParameterDomain.com";
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+          output.TagName= "a";
+          var content = await output.GetChildContentAsync();
+          var target = content.GetContent() + "@" + EamilDomain;
+          output.Attributes.SetAttribute("href", "mailto" + target);
+          output.Content.SetContent(target);
+
+        }
+    }
+}
